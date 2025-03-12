8e6568c0f584de3268adf589cbf67aac265ff3fa
sq1533 <618356@naver.com>
2025-03-12 19:03:16 +0900

    feat : 250312_v0.02 telegramBot API

--- telegramBot.py
+++ telegramBot.py
@@ -6,30 +6,23 @@ streamlit.set_page_config(
                         page_title='howToUsed_telegramBot-API',
                         page_icon='',
                         layout='wide',
-                        initial_sidebar_state='collapsed',
-                        menu_items={
-                                    'telegramBotAPI':'https://core.telegram.org/bots'
-                                    }
+                        initial_sidebar_state='collapsed'
                         )
-
-#세션관리
-if 'botAPI' not in streamlit.session_state:
-    streamlit.session_state['botAPI'] = None
-if 'chatID' not in streamlit.session_state:
-    streamlit.session_state['chatID'] = None
 
 streamlit.header(body="telegramBot_API 활용")
 
 #텔레그램 봇 API 채널 확인
 streamlit.subheader(body="telegramBot chatID값 확인하기")
 
-inputAPI = streamlit.text_input(label='bot_API',
-                     value='',
-                     key='botAPI_chatID',
-                     placeholder='telegramBotAPI 입력',
-                     label_visibility='visible')
+inputAPI = streamlit.text_input(
+                                label='bot_API',
+                                value='',
+                                key='botAPI_chatID',
+                                placeholder='telegramBotAPI 입력',
+                                label_visibility='visible'
+                                )
 
-if streamlit.button(label='조회',key='chatID'):
+if streamlit.button(label='조회',key='chatIDButton'):
     streamlit.session_state['botAPI'] = inputAPI
     response = requests.get(url=f'https://api.telegram.org/bot{inputAPI}/getUpdates')
     if response.json()['ok'] == True:
@@ -39,41 +32,32 @@ if streamlit.button(label='조회',key='chatID'):
     else:
         streamlit.warning(body=f"요청 실패, {response}")
 
-streamlit.code(body=
-                """
-                inputAPI = streamlit.text_input(label='bot_API',
-                                                value='',
-                                                key='botAPI_chatID',
-                                                placeholder='telegramBotAPI 입력')
-
-                if streamlit.button(label='조회',key='chatID'):
-                    response = requests.get(url=f'https://api.telegram.org/bot{inputAPI}/getUpdates')
-                    if response.json()['ok'] == True:
-                        chatID = response.json()['result'][-1]['message']['chat']['id']
-                        streamlit.write(f"봇 채널 ID : **{chatID}**")
-                    else:
-                        streamlit.warning(body=f"요청 실패, {response}")
-                """)
-
-#텔레그램 봇 API text보내기
-streamlit.subheader(body="telegramBot Text 데이터 전송")
-with streamlit.expander(label='Text 데이터 전송'):
+text, image = streamlit.columns(spec=2, gap='small', vertical_alignment='center')
+#텔레그램 봇 API 데이터 보내기
+with text.container():
+    streamlit.subheader(body="text 데이터 전송")
     API, chatID = streamlit.columns(spec=[3,1], gap='small', vertical_alignment='center')
 
-    bot_API = API.text_input(label='bot_API',
-                   value=streamlit.session_state['botAPI'],
-                   key='botAPI_sendText',
-                   label_visibility='visible')
+    bot_API = API.text_input(
+                            label='bot_API',
+                            value='',
+                            key='botAPI_sendText',
+                            label_visibility='visible'
+                            )
     
-    chat_id = chatID.text_input(label='chatID',
-                   value=streamlit.session_state['chatID'],
-                   key='chatID_sendText',
-                   label_visibility='visible')
+    chat_id = chatID.text_input(
+                                label='chatID',
+                                value='',
+                                key='chatID_sendText',
+                                label_visibility='visible'
+                                )
     
-    textData = streamlit.text_input(label='text입력',
-                   value='',
-                   key='value_sendText',
-                   label_visibility='visible')
+    textData = streamlit.text_area(
+                                    label='text입력',
+                                    value='',
+                                    key='value_sendText',
+                                    label_visibility='visible'
+                                    )
     
     if streamlit.button(label='전송',key='sendText'):
         response = requests.get(url=f'https://api.telegram.org/bot{bot_API}/sendMessage?chat_id={chat_id}&text={textData}')
@@ -82,32 +66,33 @@ with streamlit.expander(label='Text 데이터 전송'):
         else:
             streamlit.write(f'전송 실패, {response}')
 
-    streamlit.code(body=
-                    """
-                    requests.post(url=f'https://api.telegram.org/bot{bot_API}/sendMessage?chat_id={chat_id}&text={textData}')
-                    """)
-
 #이미지 전송
-streamlit.subheader(body="telegramBot 이미지 데이터 전송")
-with streamlit.expander(label='이미지 데이터 전송'):
+with image.container():
+    streamlit.subheader(body="이미지 데이터 전송")
     API, chatID = streamlit.columns(spec=[3,1], gap='small', vertical_alignment='center')
 
-    bot_API = API.text_input(label='bot_API',
-                   value=streamlit.session_state['botAPI'],
-                   key='botAPI_sendImage',
-                   label_visibility='visible')
+    bot_API = API.text_input(
+                            label='bot_API',
+                            value='',
+                            key='botAPI_sendImage',
+                            label_visibility='visible'
+                            )
     
-    chat_id = chatID.text_input(label='chatID',
-                   value=streamlit.session_state['chatID'],
-                   key='chatID_sendImage',
-                   label_visibility='visible')
+    chat_id = chatID.text_input(
+                                label='chatID',
+                                value='',
+                                key='chatID_sendImage',
+                                label_visibility='visible'
+                                )
 
-    file = streamlit.file_uploader(label='image',
-                                   type=['jpg','jpeg','png'],
-                                   help='200MB이하, JPG 및 JPEG, PNG 파일',
-                                   accept_multiple_files=False,
-                                   key='value_sendImage',
-                                   label_visibility='visible')
+    file = streamlit.file_uploader(
+                                    label='image',
+                                    type=['jpg','jpeg','png'],
+                                    help='200MB이하, JPG 및 JPEG, PNG 파일',
+                                    accept_multiple_files=False,
+                                    key='value_sendImage',
+                                    label_visibility='visible'
+                                    )
 
     if streamlit.button(label='전송',key='sendImage'):
         response = requests.post(url=f'https://api.telegram.org/bot{bot_API}/sendDocument', data={"chat_id":chat_id}, files={"document":file})
@@ -115,8 +100,3 @@ with streamlit.expander(label='이미지 데이터 전송'):
             streamlit.write('전송 성공')
         else:
             streamlit.write(f'전송 실패, {response}')
-
-    streamlit.code(body=
-                    """
-                    requests.post(url=f'https://api.telegram.org/bot{bot_API}/sendDocument', data={"chat_id":chat_id}, files={"document":file})
-                    """)
