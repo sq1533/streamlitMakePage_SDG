4d6187befa2f67e36d3a36a1bccde37bca6f15e2
sq1533 <618356@naver.com>
2025-03-14 19:40:24 +0900

    feat : 250314_v0.07 sendMessage, 로딩 표현 추가

--- main.py
+++ main.py
@@ -18,32 +18,55 @@ async def home(request:Request):
 #chatID 확인
 @app.post("/chatID")
 async def ChatID(request : Request, botAPI : str = Form(...)):
+    failResultHtml = '<div id="chatIDresult">요청 실패</div>'
     result = await telegramBotAPI.getChatID(botAPI=botAPI)
     if result and result['ok'] == True:
         chatID = result['result'][-1]['message']['chat']['id']
         return templates.TemplateResponse(request=request,name="output.html",context={'botAPI':botAPI,'chatID':chatID})
     else:
-        return HTMLResponse(content=f"<div>요청 실패</div>")
+        return HTMLResponse(content=failResultHtml)
 
 #텍스트 데이터 전송
 @app.post("/sendText")
-async def telebot_sendText(request : Request, botAPI : str = Form(...), chatID : str = Form(...), sendText : str = Form(...)):
-    result = await telegramBotAPI.sendText(botAPI=botAPI, chatID=chatID, sendText=sendText)
+async def telebot_sendText(botAPI : str = Form(...), chatID : str = Form(...), text : str = Form(...)):
+    successResultHtml = '<div id="sendTextResult">요청 성공</div>'
+    failResultHtml = '<div id="sendTextResult">요청 실패</div>'
+    result = await telegramBotAPI.sendText(
+                                        botAPI = botAPI,
+                                        chatID = chatID,
+                                        text = text
+                                        )
     if result and result['ok'] == True:
-        return templates.TemplateResponse(request=request,name="output.html",context={'botAPI':botAPI,'chatID':chatID})
+        return HTMLResponse(content=successResultHtml)
     else:
-        return HTMLResponse(content=f"<div>요청 실패</div>")
+        return HTMLResponse(content=failResultHtml)
 
-#텍스트(옵션버튼 추가) 전송
-@app.post("/sendTextOption")
-async def telebot_sendText_Option(botAPI : str = Form(...), chatID : str = Form(...), mainText : str = Form(...), buttonText : str = Form(...), callback : str = Form(...)):
-    result = await telegramBotAPI.sendTextPotion(botAPI=botAPI, chatID=chatID, mainText=mainText, buttonText=buttonText, callback=callback)
+#텍스트_inline 데이터 전송
+@app.post("/sendTextWithInline")
+async def telebot_sendText_withInline(botAPI : str = Form(...), chatID : str = Form(...), text : str = Form(...), buttonText : str = Form(...), callback : str = Form(...)):
+    successResultHtml = '<div id="sendTextWithLinlineResult">요청 성공</div>'
+    failResultHtml = '<div id="sendTextWithLinlineResult">요청 실패</div>'
+    result = await telegramBotAPI.sendText_withInline(
+                                        botAPI = botAPI,
+                                        chatID = chatID,
+                                        text = text,
+                                        buttonText=buttonText,
+                                        callback=callback
+                                        )
     if result and result['ok'] == True:
-        return HTMLResponse(content="<div>전송 완료</div>")
+        return HTMLResponse(content=successResultHtml)
     else:
-        return HTMLResponse(content=f"<div>요청 실패</div>")
+        return HTMLResponse(content=failResultHtml)
 
-""" 422응답 오류, 추후 재시도
+"""
+@app.get("/add_input{count}")
+def add_input(count:int):
+    replyMarkup = f"input_html"
+    if count < 6:
+        return HTMLResponse(content=replyMarkup)
+    else:
+        return HTMLResponse(content=f"<div>최대 5개 추가</div>")
+
 #이미지 데이터 전송
 @app.post("/sendImage")
 async def getChatID(botAPI : str = Form(...), chatID : str = Form(...), sendImage : UploadFile = File(...)):
