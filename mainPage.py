67739bfc0d41a3423b0e32790b7909852f626ffa
sq1533 <618356@naver.com>
2025-04-15 16:48:26 +0900

    feat : mainPage Title 설정

--- mainPage.py
+++ mainPage.py
@@ -1,6 +1,14 @@
 import streamlit as st
 
-st.write("""
-# My first app
-Hello *world!*
-""")
+# 페이지 기본 설정
+st.set_page_config(
+    page_title="shop_demo",
+    page_icon=":shark:",
+    layout="wide",
+    initial_sidebar_state="expanded",
+    menu_items=None
+)
+
+st.title(
+    body="shop_demo"
+)
