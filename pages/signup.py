4571e1705598a7f165bde7a9a5ba78ecc7a80304
DDDG <618356@naver.com>
2025-07-28 06:35:44 +0900

    feat : signUP 단계 수정

--- pages/signup.py
+++ pages/signup.py
@@ -1,18 +1,27 @@
 import streamlit as st
+from email_validator import validate_email, EmailNotValidError
 import time
-from utils import auth
+import utils
 
-# 세션 정의
+# 회원 가입 구분
 if "signup_step" not in st.session_state:
     st.session_state.signup_step = False
+
+# 
 if "user_status" not in st.session_state:
     st.session_state.user_status = False
+
+# 
 if "signup_email" not in st.session_state:
     st.session_state.signup_email = None
 
 # 세션 정보 검증 및 이메일 검증
 if st.session_state.signup_step:
+    # 사이드바 내용
+    with st.sidebar:
+        st.title("환영합니다.")
 
+    # 메인 페이지
     empty, main, empty = st.columns(spec=[1,4,1], gap="small", vertical_alignment="top")
 
     with main.container():
@@ -26,8 +35,7 @@ if st.session_state.signup_step:
         )
         if goHome:
             st.switch_page(page="mainPage.py")
-        with st.sidebar:
-            st.title("환영합니다.")
+
         if "signupStep" not in st.query_params:
             st.progress(
                 value=0,
@@ -42,20 +50,30 @@ if st.session_state.signup_step:
                 help=None,
                 placeholder="id@email.com"
             )
-            user_info = None
+            nextBTN = True
+            checkEmail = st.button(
+                label="다음",
+                key="goMakePW",
+                type="primary",
+                use_container_width=True,
+                disabled=nextBTN
+            )
             if email:
                 with st.spinner(text="이메일을 확인해볼게요", show_time=True):
                     try:
-                        user_info = auth.get_user_by_email(email=email)
-                        if user_info.email_verified:
-                            st.session_state.user_status = "verified"
-                        else:
-                            st.session_state.user_status = "unverified"
-                    except auth.UserNotFoundError:
-                        st.session_state.user_status = "not_found"
-                    except Exception:
-                        st.error("사용자 확인 중 오류 발생")
-                        st.session_state.user_status = None
+                        if validate_email(email=email, check_deliverability=True, timeout=5)['email'] == email:
+                            userEmail = utils.guest.emailCheck(id=email)
+                            if userEmail: # 신규 회원일 경우
+                                nextBTN = False
+                            else: # 회원가입 후 이메일 인증 안된경우, 회원가입 후 이메일 인증까지 완료한 경우
+                                pass
+
+                    except EmailNotValidError as e:
+                        print(e)
+                        st.error(
+                            body='유효하지 않은 이메일 형식입니다.'
+                        )
+
                 # 회원 DB 검증
                 if st.session_state.user_status == "verified":
                     st.error("이미 가입한 이메일입니다. 확인해주세요.")
